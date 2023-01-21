fc666e54a47c9ad39d8d56475b8df7284fb3e500
jelenajoksa <jelena.joksimovic92@gmail.com>
2023-01-21 13:42:16 +0100

    clustering done and plots

--- src/clustering.py
+++ src/clustering.py
@@ -27,18 +27,16 @@ def clustering(config_path):
     df, whole_df = get_data(config_path)
     df_num = df.iloc[:, :-1]
     arr_df = df_num.to_numpy()
-    X_train = TimeSeriesScalerMeanVariance().fit_transform(arr_df)
+    x_train = TimeSeriesScalerMeanVariance().fit_transform(arr_df)
     no_clusters = config['clustering']['no_clusters']
     algo = TimeSeriesKMeans(n_clusters=no_clusters, verbose=True, random_state=config['clustering']['random_state'])
-    y_pred = algo.fit_predict(X_train)
-    n, m, k = X_train.shape
-    arr_X_train = np.reshape(X_train, (n, m))
-    silhouette_avg = silhouette_score(arr_X_train, y_pred)
+    y_pred = algo.fit_predict(x_train)
+    n, m, k = x_train.shape
+    arr_x_train = np.reshape(x_train, (n, m))
+    silhouette_avg = silhouette_score(arr_x_train, y_pred)
     labels = algo.labels_
     companies = df.iloc[:, -1]
     #KMeans + Euclidean
-    algo = TimeSeriesKMeans(n_clusters = no_clusters, verbose=True, random_state=10)
-    y_pred = algo.fit_predict(X_train)
     #add received labels to cutoff dataset
     whole_df = whole_df.iloc[:, :-1]
     df_num['podjetje'] = companies
@@ -48,24 +46,23 @@ def clustering(config_path):
     clusters_df = merge_df.iloc[:, -211:]
     clustered_data_path = config['processed_data']['clustered_data']
     clusters_df.to_csv(clustered_data_path, sep=",", index=False)
-    return algo, X_train, y_pred, silhouette_avg
+    return algo, x_train, y_pred, silhouette_avg
 
 #Output1: Plots
 
 #We plot 10 separate figures with 10 clusters in each, for the better preview
 def km_plot(config_path):
-    algo, X_train, y_pred, silhouette_avg = clustering(config_path)
-    for i in range(1,4):
+    config = read_params(config_path)
+    algo, x_train, y_pred, silhouette_avg = clustering(config_path)
+    for i in range(1,11):
         plt.figure()
         for yi in range((i-1)*10,(i-1)*10+10):
                 plt.subplot(2,5,yi-(i-1)*10+1)
-                for xx in X_train[y_pred == yi]:
+                for xx in x_train[y_pred == yi]:
                     plt.plot(xx.ravel(), "k-", alpha=.05)
                 plt.plot(algo.cluster_centers_[yi].ravel(), "r-")
                 plt.axvline(x=6, color='b', ls='--', lw=1)
                 plt.text(-3, -3, 'Cluster %d' % (yi))
-                if yi == 1:
-                   plt.title("Euclidean $k$-means (MeanVar)\n"+str(round(silhouette_avg,3)))
         plt.savefig('plots/kmeans/event_2014/clusters_{}.png'.format(i))
 
 
